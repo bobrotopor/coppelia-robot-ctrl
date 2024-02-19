6a155097b61ed1d004cefba0af0dfe38b9e8ccd4
bobrotopor <kaster114@gmail.com>
2024-02-19 18:01:39 +0300

    промежуточный коммит

--- hm_2/run.py
+++ hm_2/run.py
@@ -3,13 +3,6 @@ import sim.sim as sim
 import numpy as np
 
 
-dh_params = [{'a':  0,      'alpha':    90*deg,     'd':    89,     'theta':    0},
-             {'a':  -425,   'alpha':    0,          'd':    0,      'theta':    -90*deg},
-             {'a':  -392,   'alpha':    0,          'd':    0,      'theta':    0},
-             {'a':  0,      'alpha':    90*deg,     'd':    109,    'theta':    -90*deg},
-             {'a':  0,      'alpha':    -90*deg,    'd':    94,     'theta':    0},
-             {'a':  0,      'alpha':    0,          'd':    82,     'theta':    0}]
-
 def init_client_id():
     sim.simxFinish(-1)  # just in case, close all opened connections
     client_id = sim.simxStart(
@@ -30,34 +23,16 @@ def init_client_id():
     return client_id
 
 
-def get_q_ids(client_id):
-    """Получить список id обобщённых координат манипулятора."""
-    q_ids = np.array([0, 0, 0, 0, 0])
-    err, q_ids[0] = sim.simxGetObjectHandle(client_id, 'j_1', sim.simx_opmode_oneshot_wait)
-    err, q_ids[1] = sim.simxGetObjectHandle(client_id, 'j_2', sim.simx_opmode_oneshot_wait)
-    err, q_ids[2] = sim.simxGetObjectHandle(client_id, 'j_3', sim.simx_opmode_oneshot_wait)
-    err, q_ids[3] = sim.simxGetObjectHandle(client_id, 'j_4', sim.simx_opmode_oneshot_wait)
-    err, q_ids[4] = sim.simxGetObjectHandle(client_id, 'j_5', sim.simx_opmode_oneshot_wait)
-    return q_ids
 
-
-def get_q(client_id, j_ids):
-    """Получить список текущих значений обобщённых координат манипулятора."""
-    q = np.zeros(5)
-    error, q[0] = sim.simxGetJointPosition(client_id, j_ids[0], sim.simx_opmode_oneshot_wait)
-    error, q[1] = sim.simxGetJointPosition(client_id, j_ids[1], sim.simx_opmode_oneshot_wait)
-    error, q[2] = sim.simxGetJointPosition(client_id, j_ids[2], sim.simx_opmode_oneshot_wait)
-    error, q[3] = sim.simxGetJointPosition(client_id, j_ids[3], sim.simx_opmode_oneshot_wait)
-    error, q[4] = sim.simxGetJointPosition(client_id, j_ids[4], sim.simx_opmode_oneshot_wait)
-    return q
-
-
-def move(client_id, q_ids, q):
-    error = sim.simxSetJointTargetPosition(client_id, q_ids[0], q[0], sim.simx_opmode_streaming)
-    error = sim.simxSetJointTargetPosition(client_id, q_ids[1], q[1], sim.simx_opmode_streaming)
-    error = sim.simxSetJointTargetPosition(client_id, q_ids[2], q[2], sim.simx_opmode_streaming)
-    error = sim.simxSetJointTargetPosition(client_id, q_ids[3], q[3], sim.simx_opmode_streaming)
-    error = sim.simxSetJointTargetPosition(client_id, q_ids[4], q[4], sim.simx_opmode_streaming)
+def get_transform(dh_params: dict, ):
+    dh_params
+    d, a, thetta, alpha
+    return np.array([
+        [c(thetta), -c(alpha)*s(thetta), s(alpha)*s(thetta), a*c(thetta)],
+        [s(thetta), c(alpha)*c(thetta), -s(alpha)*c(thetta), a*s(thetta)],
+        [0, s(alpha), c(alpha), d],
+        [0, 0, 0, 1]
+    ])
 
 
 
@@ -70,7 +45,7 @@ if __name__ == '__main__':
 
     poses_1 = get_q(client_id=id, j_ids=joints)
 
-    q = [1, 1, 1.57, 1.57, 1]
+    q = [0, 0, 0, 0, 0]
     move(id, joints, q)
     poses_2 = get_q(client_id=id, j_ids=joints)
 
